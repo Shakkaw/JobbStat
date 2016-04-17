6930667c582d9687839fafffa5b53912dbeee163
Andreas M <andreas@amkreativ.se>
2016-04-17 16:39:45 +0200

    Object generation

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -24,6 +24,10 @@ angular.module('angularApp')
           $scope.dataset2 = data;
         });
 
+    $http.get('./data/jobbstat_region.json').success(function(data) {
+          $scope.regiondata = data;
+        });
+
     $scope.selectedID = '0000';
 
     $scope.labels = ['book','book','pencil','university','cog','industry','leaf','heart','briefcase','question'];
@@ -40,6 +44,10 @@ angular.module('angularApp')
 
     };
 
+    $scope.wewlad = function(){
+      return 'aaa';
+    };
+
     $scope.createDataset = function(){
       $scope.totaldata = {
 
@@ -50,6 +58,37 @@ angular.module('angularApp')
         $scope.totaldata[key] = $scope.create(key);
         $scope.totaldata[key].label = $scope.dataset.dataset.dimension.Yrke2012.category.label[key];
 
+
+      }
+      console.log($scope.regiondata.dataset.dimension.Region.category.index);
+      for (key in $scope.regiondata.dataset.dimension.Region.category.index) {
+        console.log(key + ' ' + $scope.regiondata.dataset.dimension.Region.category.index[key] + ' ' + $scope.regiondata.dataset.dimension.Region.category.label[key]);
+        
+        var currentRegion = {
+          index: $scope.regiondata.dataset.dimension.Region.category.index[key],
+          label: $scope.regiondata.dataset.dimension.Region.category.label[key]
+        };
+
+        var regionStart = $scope.regiondata.dataset.dimension.Region.category.index[key] * 860;
+        var regionEnd = $scope.regiondata.dataset.dimension.Region.category.index[key] * 860 + 860;
+
+        var currentData = $scope.regiondata.dataset.value.slice(regionStart,regionEnd);
+
+        console.log(currentRegion);
+        console.log(currentData);
+
+
+        //console.log(currentData);
+        for (var key2 in $scope.regiondata.dataset.dimension.Yrke2012.category.index) {
+          //$scope.totaldata[key2].region[key] = {total: 0, man: 0, kvinnor: 0};
+          var total = currentData[$scope.regiondata.dataset.dimension.Yrke2012.category.index[key2]*2 + $scope.regiondata.dataset.dimension.Yrke2012.category.index[key2]*2+1];
+          var man = currentData[$scope.regiondata.dataset.dimension.Yrke2012.category.index[key2]*2];
+          var kvinnor = currentData[$scope.regiondata.dataset.dimension.Yrke2012.category.index[key2]*2+1];
+
+
+          $scope.totaldata[key2].region[key] = {total: total, man: man, kvinnor: kvinnor};
+
+        }
       }
 
       console.log($scope.totaldata);
@@ -57,19 +96,25 @@ angular.module('angularApp')
 
     $scope.create = function(selected){
       $scope.currentObject = {
+        id: 0,
         total:0,
         totalMan:0,
         totalKvinnor:0,
         maxVal:0,
         rowVal:[0,0,0,0,0,0,0,0,0,0],
         columnVal:[0,0,0,0,0,0,0,0,0],
-        cell:[]
+        cell:[],
+        regionMaxMan:0,
+        regionMinMan:0,
+        regionMaxKvinnor:0,
+        regionMinKvinnor:0,
+        region:[]
 
       };
       $scope.selectedID = selected;
       $scope.objectData1 =  $scope.dataset.dataset.value.slice($scope.getCurrentIndex()*90,$scope.getCurrentIndex()*90+90);
       $scope.objectData2 =  $scope.dataset2.dataset.value.slice($scope.getCurrentIndex()*90,$scope.getCurrentIndex()*90+90);
-
+      $scope.currentObject.id = selected;
       //console.log($scope.objectData1);
       //console.log($scope.objectData2);
 
@@ -90,11 +135,14 @@ angular.module('angularApp')
         $scope.currentObject.totalKvinnor += currentCell.kvinnor;
         $scope.currentObject.columnVal[currentCell.column] += currentCell.total;
         $scope.currentObject.rowVal[currentCell.row] += currentCell.total;
+        //console.log(selected);
 
         if (currentCell.total > $scope.currentObject.maxVal) {
           //console.log('Maxval: ' + currentCell.total);
           $scope.currentObject.maxVal = currentCell.total;
         }
+
+
       }
 
       //console.log($scope.currentObject);
@@ -102,8 +150,13 @@ angular.module('angularApp')
       
     };
 
-
     $scope.select = function(selected){
+      console.log($scope.totaldata[selected]);
+      $scope.currentObject = $scope.totaldata[selected];
+    };
+
+
+    $scope.select2 = function(selected){
       $scope.currentObject = {
         total:0,
         totalMan:0,
