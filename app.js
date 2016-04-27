438adb25f371036cb4a8c845eb366189119351ae
Andreas M <andreas@amkreativ.se>
2016-04-27 12:27:19 +0200

    Testing out stuff

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/app'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
