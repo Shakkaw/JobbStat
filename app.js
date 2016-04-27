ad7cab41757eb69ff5063e1f788c6588d6b4764a
Andreas M <andreas@amkreativ.se>
2016-04-27 12:14:28 +0200

    wew lad 3

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ var port = process.env.PORT || 3000;
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/dist'));
 
-/*app.get('/', function (req, res) {
+app.get('/', function (req, res) {
   res.send('Hello World!');
 });
-*/
+
 
 app.listen(port, function () {
   console.log('Example app listening on port 3000!');
-  console.log(express.static(__dirname + '/dist'));
+  //console.log(express.static(__dirname + '/dist'));
 });
